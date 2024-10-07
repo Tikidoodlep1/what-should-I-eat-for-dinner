04ac626ac4349b1f6737db23e7df248022614e4d
Tikidoodlep1 <alex.x.eye@gmail.com>
2024-10-07 11:51:40 -0500

    Added index.php, upadted index.html

--- index.php
+++ index.php
@@ -3,23 +3,29 @@
 <html lang="en-US">
 <head>
     <meta charset="UTF-8">
-    <title>What Should I Eat For Dinner</title>
+    <?php
+    	$title = basename($_SERVER['PHP_SELF'], ".php");
+    	if($title === "index") {
+    		$title = "What Should I Eat For Dinner";
+    	}
+    	echo "<title>$title</title>";
+    ?>
     <link rel="stylesheet" href="./main_style.css">
 </head>
 <body>
 	<!-- NAV BANNER -->
 	<header>
-		<nav>
-			<div class="banner">
-				<h1 id="title">What Should I Eat For Dinner</h1>
+		<div class="banner">
+			<h1 id="title">What Should I Eat For Dinner</h1>
+			<nav>
 				<ul>
 					<!-- Insert different pages here for searching, saved searches, saves recipes, etc if we implement them -->
 				</ul>
-			</div>
-		</nav>
+			</nav>
+		</div>
 	</header>
 	<main>
-		<!--  -->
+		<!-- Checkboxes, body stuff -->
 	</main>
 </body>
 </html>
