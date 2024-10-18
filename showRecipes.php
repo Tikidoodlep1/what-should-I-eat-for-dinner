407d6534259b205088c58d46a261783a66af1aa7
Tikidoodlep1 <alex.x.eye@gmail.com>
2024-10-18 15:17:35 -0500

    Put the recipes in a table - needs styling

--- showRecipes.php
+++ showRecipes.php
@@ -30,7 +30,7 @@ $db = new DbUtils();
 $db->SetErrorReporting(true);
 //We should add two GenerateRecipes buttons - one to call QueryRecipesIncludingIngredients and one to call QueryRecipesWithIngredients.
 $searchResults = $db->QueryRecipesIncludingIngredients($ingredients);
-var_dump($searchResults);
+//var_dump($searchResults);
 ?>
 
 <!DOCTYPE html>
@@ -45,16 +45,44 @@ var_dump($searchResults);
         <p><?php echo htmlspecialchars($description); ?></p>
     </header>
     <main>
-        <h2>Your Selected Ingredients:</h2>
-        <ul>
-            <?php if (!empty($ingredients)): ?>
-                <?php foreach ($ingredients as $ingredient): ?>
-                    <li><?php echo htmlspecialchars(trim($ingredient)); ?></li>
-                <?php endforeach; ?>
-            <?php else: ?>
-                <li>No ingredients were selected!</li>
-            <?php endif; ?>
-        </ul>
+        <section id="selected-ingredients">
+            <h2>Your Selected Ingredients:</h2>
+            <ul>
+                <?php if (!empty($ingredients)) { ?>
+                    <?php foreach ($ingredients as $ingredient): ?>
+                        <li><?php echo htmlspecialchars(trim($ingredient)); ?></li>
+                    <?php endforeach; ?>
+                <?php }else { ?>
+                    <li>No ingredients were selected!</li>
+                <?php } ?>
+            </ul>
+        </section>
+        <section id="recipes-table">
+            <table>
+                <thead>
+                    <th>Recipe</th>
+                    <th>Ingredients</th>
+                    <th>Instructions</th>
+                </thead>
+                <tbody>
+                    <?php
+                        for($i = 0; $i < count($searchResults["id"]); $i++) {
+                    ?>
+                        <tr>
+                    <?php
+                        $imgData = "<td><img loading='lazy' src='./img/" . $searchResults["img_name"][$i] . ".jpg'></td>";
+                        $titleData = "<td class='recipe-title'>" . $searchResults["title"][$i] . "</td>";
+                        $ingredientsData = "<td class='recipe-ingredients'>" . $searchResults["ingredients"][$i] . "</td>";
+                        $instructionsData = "<td class='recipe-instructions'>" . $searchResults["instructions"][$i] . "</td>";
+                        echo $imgData . $titleData . $ingredientsData . $instructionsData;
+                    ?>
+                        </tr>
+                    <?php
+                        }    
+                    ?>
+                </tbody>
+            </table>
+        </section>
     </main>
 </body>
 </html>
